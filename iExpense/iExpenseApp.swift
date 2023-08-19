bba5fe56cbd35730ba2c1e1e4df6a4539d10de7b
Eugene Cross <eugene.cross@gmail.com>
2023-08-19 22:27:38 +0100

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Eugene on 19/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
